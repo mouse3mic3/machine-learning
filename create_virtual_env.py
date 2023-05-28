68649ea804d60630534cbb576e0826ad3307159a
mzakin <mouse3mic3@gmail.com>
2023-05-28 17:51:22 +0700

    'added_gammafest'

--- create_virtual_env.py
+++
@@ -1,9 +0,0 @@
-from venv import create
-from os.path import join, expanduser, abspath
-from subprocess import run
-
-dir = join(expanduser("~"), "my-venv")
-create(dir, with_pip=True)
-
-# where requirements.txt is in same dir as this script
-run(["bin/pip", "install", "-r", abspath("requirements.txt")], cwd=dir)
